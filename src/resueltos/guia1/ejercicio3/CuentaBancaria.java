c871b463e454d81641ccf960bff44d48fcd182cd
Charly Cimino <carlosecimino@gmail.com>
2023-04-14 14:40:23 -0300

    EJ3 corregido en clase

--- src/resueltos/guia1/ejercicio3/CuentaBancaria.java
+++ src/resueltos/guia1/ejercicio3/CuentaBancaria.java
@@ -4,18 +4,25 @@
  */
 package resueltos.guia1.ejercicio3;
 
+import java.util.Random;
+
 /**
  *
  * @author karol
  */
 public class CuentaBancaria {
     private String CBU; //CBU como string, porque 22 dígitos es imposible como dato numérico (?
-    private String tipo_cuenta;
+    private String tipoCuenta;
     private double saldo; //Salvo que se aclare, es 0 por default.
-    
+
+    public CuentaBancaria(String tipoCuenta) {
+        this.CBU = generarCBU();
+        this.saldo = 0;
+        this.tipoCuenta = tipoCuenta;
+    }
    
-    public void Depositar(double monto){
-        if(monto >0){
+    public void depositar(double monto){
+        if(monto > 0){
             saldo += monto;
         }
         else{
@@ -23,46 +30,49 @@ public class CuentaBancaria {
         }
     }
     
-    public void Extraer(double monto){
-        if(monto >0 && tipo_cuenta == "corriente"){
-            saldo -= monto;
-        }
-        
-        else if(monto>0 && tipo_cuenta == "ahorro"){
-            if(monto <=saldo){
-                saldo -=monto;
+    public void extraer(double monto){
+        if(monto > 0){
+            if( tipoCuenta.equals("corriente") || monto <= saldo) {
+                saldo -= monto;
             }
-            else{
+            else {
                 System.out.println("Su saldo disponible no permite extraer ARS "+ monto);
             }
+        } else{
+            System.out.println("El monto a depositar debe ser un real positivo");
         }
+        
+        
     }
     
     public double getSaldoActual(){
         return saldo;
-    }
+    }    
     
-    public void setCBU(String cbu){
-        CBU = cbu;
-    }
-    
-    public void setTipoCuenta(String tipo){
-        tipo_cuenta = tipo;
-    }
-    
-    public String getTipoCuenta(){
-        return tipo_cuenta;
-    }
-    
-    
-    public void getUltimosTresCBU(){
-        String ultimos = CBU.substring(19);
+    public void getUltimosCBU(int cuantos){
+        String ultimos = CBU.substring(CBU.length() - cuantos);
         System.out.println("Últimos 3 dígitos del CBU: " + ultimos);
     }
     
     //agrego un método que muestre los datos ingresados en prog ppal.
-    public void MostrarDatos(){
-        System.out.println("CBU: " + CBU + "\nTipo de cuenta: " + tipo_cuenta + "\nSaldo actual: "+saldo);
+    public void mostrarDatos(){
+        System.out.println(toString());
     }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria{" + "CBU=" + CBU + ", tipoCuenta=" + tipoCuenta + ", saldo=" + saldo + '}';
+    }
+
+    private String generarCBU() {
+        Random r = new Random();
+        String elCbu = "";
+        for (int i = 0; i < 22; i++) {
+            elCbu += r.nextInt(10);
+        }
+        return elCbu;        
+    }
+    
+    
     
 }
--- src/resueltos/guia1/ejercicio3/Principal.java
+++ src/resueltos/guia1/ejercicio3/Principal.java
@@ -15,40 +15,36 @@ public class Principal {
      */
     public static void main(String[] args) {
         
-        CuentaBancaria cuenta1 = new CuentaBancaria();
-        CuentaBancaria cuenta2 = new CuentaBancaria();
+        CuentaBancaria cuenta1 = new CuentaBancaria("corriente");
+        CuentaBancaria cuenta2 = new CuentaBancaria("ahorro");
         
         //ingreso el CBU:
-        cuenta1.setCBU("0087645390000487529643");
-        cuenta1.setTipoCuenta("corriente");
-        cuenta1.MostrarDatos();
-        
-        cuenta2.setCBU("2647895462146325");
-        cuenta2.setTipoCuenta("ahorro");
+        cuenta1.mostrarDatos();
+        cuenta2.mostrarDatos();
         
         
-        cuenta1.Depositar(3570);
-        
-        cuenta1.MostrarDatos();
-        
-        cuenta1.Extraer(370);
-        
-        System.out.println("Saldo actual: ARS " + cuenta1.getSaldoActual());
-        
-        cuenta1.Extraer(5000);
-        
-        System.out.println("Saldo actual: ARS " + cuenta1.getSaldoActual());
-        cuenta1.Depositar(860);
-        System.out.println("Saldo actual: ARS " + cuenta1.getSaldoActual());
-        cuenta1.getUltimosTresCBU();
-        System.out.println("Tipo de cuenta: " + cuenta1.getTipoCuenta());
-        
-        cuenta2.MostrarDatos();
-        cuenta2.Depositar(1650);
-        cuenta2.MostrarDatos();
-        
-        cuenta2.Extraer(1800);
-        cuenta2.MostrarDatos();
+//        cuenta1.depositar(3570);
+//        
+//        cuenta1.mostrarDatos();
+//        
+//        cuenta1.extraer(370);
+//        
+//        System.out.println("Saldo actual: ARS " + cuenta1.getSaldoActual());
+//        
+//        cuenta1.extraer(5000);
+//        
+//        System.out.println("Saldo actual: ARS " + cuenta1.getSaldoActual());
+//        cuenta1.depositar(860);
+//        System.out.println("Saldo actual: ARS " + cuenta1.getSaldoActual());
+//        cuenta1.getUltimosTresCBU();
+//        System.out.println("Tipo de cuenta: " + cuenta1.getTipoCuenta());
+//        
+//        cuenta2.mostrarDatos();
+//        cuenta2.depositar(1650);
+//        cuenta2.mostrarDatos();
+//        
+//        cuenta2.extraer(1800);
+//        cuenta2.mostrarDatos();
     }
     
 }
