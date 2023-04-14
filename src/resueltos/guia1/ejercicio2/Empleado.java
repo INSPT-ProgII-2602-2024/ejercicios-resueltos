c6c30c6a3ced1adb7f976d29fdd442e75e9b6ed1
Charly Cimino <carlosecimino@gmail.com>
2023-04-14 14:03:09 -0300

    EJ2 corregido en clase

--- src/resueltos/guia1/ejercicio2/Empleado.java
+++ src/resueltos/guia1/ejercicio2/Empleado.java
@@ -1,48 +1,44 @@
 package resueltos.guia1.ejercicio2;
 
 public class Empleado {
-    int DNI;
-    String nombre;
-    String apellido;
-    double salariobase;
-    EstadoCivil estadocivil;
-    int hijos;
+    private int DNI;
+    private String nombre;
+    private String apellido;
+    private double salarioBase;
+    private EstadoCivil estadocivil;
+    private int hijos;
 
     public Empleado(int DNI, String nombre, String apellido, double salariobase, EstadoCivil estadocivil, int hijos) {
         this.DNI = DNI;
         this.nombre = nombre;
         this.apellido = apellido;
-        this.salariobase = salariobase;
+        this.salarioBase = salariobase;
         this.estadocivil = estadocivil;
         this.hijos = hijos;
     }
 
-    public double obtenerSalarioFinal(){
-        int extraHijos;
-        double salarioFinal;
-        if (this.hijos>3){
-            extraHijos = 10;
-        } else {
-            extraHijos = 3 * this.hijos;
-        }
+    public double obtenerSalarioFinal() {        
+        double salario = salarioBase + plusPorHijos();
+        return dctoPorEstadoCivil(salario);
+    }
+    
+    private double plusPorHijos() {
+        double porc = 0.03 * hijos;
+        if (porc > 0.1){
+            porc = 0.1;
+        } 
+        return salarioBase * porc;
+    }
 
-        salarioFinal = this.salariobase + this.salariobase * (extraHijos) / 100;
-
-        if (this.estadocivil.equals(EstadoCivil.SOLTERO)){
-            salarioFinal = salarioFinal - (salarioFinal * 0.04);
-        }
-        return salarioFinal;
+    private double dctoPorEstadoCivil(double salario) {
+        return estadocivil != EstadoCivil.SOLTERO ? salario : salario / 1.04;
     }
 
     @Override
     public String toString() {
-        return "Empleado{" +
-                "DNI=" + DNI +
-                ", nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", salariobase=" + salariobase +
-                ", estadocivil=" + estadocivil +
-                ", hijos=" + hijos +
-                '}';
+        return "Empleado{" + "DNI=" + DNI + ", nombre=" + nombre + ", apellido=" + apellido + ", salarioBase=" + salarioBase + ", estadocivil=" + estadocivil + ", hijos=" + hijos + '}';
     }
+
+    
+    
 }
--- src/resueltos/guia1/ejercicio2/Principal.java
+++ src/resueltos/guia1/ejercicio2/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
     public static void main(String[] args) {
         Empleado empleado = new Empleado(41823445, "Juan", "Dominguez", 10000, EstadoCivil.CASADO, 4);
 
-        System.out.println(empleado.toString());
+        System.out.println(empleado);
         System.out.println("Salario Final: " + empleado.obtenerSalarioFinal());
     }
 }
